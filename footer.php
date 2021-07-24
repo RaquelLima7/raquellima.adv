4d00a267db347d44ae917a4a02ff5e2fd0d95b05
RaquelLima7 <raquellimadev@gmail.com>
2021-07-23 22:42:45 -0300

    first commit raquellima.adv in php

---
+++ footer.php
@@ -0,0 +1,34 @@
+  <!-- Footer Area -->
+  <footer class="mt-5">
+      <div class="d-flex justify-content-evenly">
+        <div><img src="images/logo1.png" alt="logo" width="70" height="70">
+          <h6> Copyright ® 2020. Todos os direitos reservados</h6>
+        </div>
+        
+        <div class="align-self-center">
+          <div>
+            <i class="fab fa-facebook-square item-footer"></i>
+            <i class="fab fa-instagram-square item-footer"></i>
+            <i class="fab fa-linkedin item-footer"></i>
+            <i class="fas fa-envelope-square item-footer"></i>
+            <i class="fab fa-whatsapp-square item-footer"></i>
+          </div>
+        </div>
+      </div>
+    </footer>
+  <!-- End Footer Area -->
+</main>
+
+
+  <!-- JS Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+    </script>
+
+  <!-- AOS -->
+  <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
+  
+  </body>
+</html>
